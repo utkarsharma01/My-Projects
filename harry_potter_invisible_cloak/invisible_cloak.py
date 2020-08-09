619f3b6930cd6910a07a0b9911235544a4db2123
utkarsharma01 <40605660+utkarsharma01@users.noreply.github.com>
2020-08-09 23:10:35 +0530

    Add files via upload

---
+++ harry_potter_invisible_cloak/invisible_cloak.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+back = cv2.imread('./image.jpg')
+
+while cap.isOpened():
+    #take each frame
+    ret, frame = cap.read()
+
+    if ret:
+        #how de we convert rgb to hsv?
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow("hsv", hsv)
+        #how to get hsv value?
+        #lower: hue - 10, 100, 100, higher: h+10, 255, 255
+        red = np.uint8([[[0,0,255]]])
+        hsv_red = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
+        #get hsv value of red
+        #print(hsv_red)
+        #threshold the hsv value to get only red colors
+        l_red = np.array([0,100,100])
+        u_red = np.array([10,255,255])
+
+        mask = cv2.inRange(hsv, l_red, u_red)
+        #cv2.imshow('mask', mask)
+
+        #part 2 is all things not red
+        part1 = cv2.bitwise_and(back,back, mask=mask)
+
+        mask = cv2.bitwise_not(mask)
+
+        part2 = cv2.bitwise_and(frame, frame, mask=mask)
+        #cv2.imshow('mask', part2)
+        cv2.imshow('cloak', part1 + part2)
+
+        if cv2.waitKey(2) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
